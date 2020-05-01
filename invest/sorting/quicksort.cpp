32bb8557b5384ccb7312565356d8e8663cf51cde
minju <52149296+minzoovv@users.noreply.github.com>
2020-05-02 02:15:32 +0900

    fix bug for quicksort

--- invest/sorting/quicksort.cpp
+++ invest/sorting/quicksort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int partition (int arr[], int start, int end){
  int pivot = arr[start];
   int k = start; 
-  for(int i = k+1; i < end ; i++ ){
+  for(int i = k+1; i <= end ; i++ ){
     if (pivot > arr[i]) {
       k++;
       swap(arr[i], arr[k]);
@@ -29,9 +29,9 @@ void quicksort(int arr[], int start, int end) {
 
 int main(void){
 
-  int arr[] = {4,6,2,1,8};
+  int arr[] = {5, 3, 99, 5, 8, 5, 39, 111, 989, 31, 88, 87, 99, 4, 187, 5, 53, 2};
 
-  quicksort(arr, 0, sizeof(arr)/sizeof(int));
+  quicksort(arr, 0, sizeof(arr)/sizeof(int)-1);
 
   for (int i = 0 ; i < sizeof(arr)/sizeof(int); i++){
     cout << arr[i] << " ";
